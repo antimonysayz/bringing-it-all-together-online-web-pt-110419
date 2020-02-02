ffa8984e21a716764e74c0c6cd6348c1ead266ef
Anthony Mai <anthony.mai530@gmail.com>
2020-02-02 20:11:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,7 +66,11 @@ class Dog
     dog = DB[:conn].execute(sql, name, breed).first
 
     if dog
-      self.new(from )
+     new_dog = self.new_from_db(dog)
+   else
+     new_dog = self.create({:name => name, :breed => breed})
+   end
+ end
 
   end
 
