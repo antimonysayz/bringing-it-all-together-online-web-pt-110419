54bd965b6eb0ab21cc294e35f666f5b25e9684c0
Anthony Mai <anthony.mai530@gmail.com>
2020-02-02 20:17:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -84,5 +84,16 @@ class Dog
    }.first
  end
 
+ def update
+   sql = <<-SQL
+   UPDATE dogs
+   SET
+   name = ?
+   breed = ?
+   WHERE id = ?
+   SQL
+   DB[:conn].execute(sql, self.name, self.breed, self.id)
+ end
+
 
 end
